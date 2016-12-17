ffb27cccdbd6af3b1cc149ec683b079538bf35d1
Matt Bowyer <mattbowyer@nwk--180-203.nwk.ruw.rutgers.edu>
2016-12-17 05:32:29 -0500

    implemented predictors in bayes network

--- BayesNetwork.py
+++ BayesNetwork.py
@@ -2,16 +2,33 @@
 
 class BayesNetwork(object):
 
-    #-----------RANDOM STARTER WEIGHTS---------------
+    #-----------RANDOM STARTER WEIGHTS--------------
 
-    def probability_y(self, X):
-        p_y = [{} for y in range(0,self.output_size)]
-        for y_index in range(len(p_y)):
-            for y_value in self.model['Y'][y_index]:
-                p_y[y_index][y_value] = (1.0*self.model['Y'][y_index][y_value])/self.model['samples']
+    def prediction_Y(self, X):
+        return self.p_Y(X)[0]
+
+    def probability_Y(self, X):
+        return self.p_Y(X)[1]
+
+    def p_Y(self, X):
+        if len(X) is not self.input_size:
+            print "Incorrect Data Dimensions"
+            return
+        probability_Y = [{} for y in range(self.output_size)]
+        prediction_Y = [0 for y in range(self.output_size)]
+        for y_index in range(len(probability_Y)):
+            prediction_Y[y_index] = self.model['Y'][y_index].keys()[0]
+            for y_value in self.model['Y'][y_index].keys():
+                p_y = (1.0*self.model['Y'][y_index][y_value])/self.model['samples']
+                p_x = 1.0
+                p_x_y = 1.0
                 for (x_index, x_value) in [(x_index, X[x_index]) for x_index in range(self.input_size)]:
-                    p_y[y_index][y_value] *= (1.0*self.model['X|Y'][y_index][y_value][x_index][x_value])/self.model['X'][x_index][x_value]
-        return p_y
+                    p_x *= (1.0*self.model['X'][x_index][x_value])/self.model['samples']
+                    p_x_y *= (1.0*self.model['X|Y'][y_index][y_value][x_index][x_value])/self.model['Y'][y_index][y_value]
+                probability_Y[y_index][y_value] = (p_x_y*p_y/p_x)
+                if probability_Y[y_index][y_value] > probability_Y[y_index][prediction_Y[y_index]]:
+                    prediction_Y[y_index] = y_index
+        return (prediction_Y, probability_Y)
 
     def forward_X(self, X):
         for (x_index, x_value) in [(x_index, X[x_index]) for x_index in range(self.input_size)]:
@@ -47,6 +64,9 @@ class BayesNetwork(object):
         self.forward_Y(Y)
         self.forward_X_Y(X,Y)
 
+    def train(self, X, Y):
+        for i in range(len(X)): self.forward(X[i], Y[i])
+
     def new_model(self):
         self.model = {
             'samples':0,
@@ -69,36 +89,19 @@ def main():
 """
 from ImageClassifier import *
 from BayesNetwork import *
-input = [[a,b,c] for a in range(3) for b in range(3) for c in range(3) for i in range(2*2)]
-output = [[a,b] for i in range(3*3*3) for a in range(2) for b in range(2)]
+import numpy as np
+
+input = np.array([[a,b,c] for a in range(3) for b in range(3) for c in range(3) for i in range(2*2)], dtype=int)
+output = np.array([[a,b] for i in range(3*3*3) for a in range(2) for b in range(2)], dtype=int)
+
 baynet = BayesNetwork(len(input[0]), len(output[0]))
-for i in range(len(input)):
-    print str(input[i]) +' ---> ' +str(output[i])
-    baynet.forward(input[i], output[i])
+baynet.train(input,output)
 
 print_struct(baynet.model)
-print_struct(baynet.probability_y([0,0,0]))
-
+print_struct(baynet.probability_Y([0,0,0]))
+print str(baynet.prediction_Y([0,0,0]))
 
 """
-
-def p_y(baynet, X):
-    p_y = [{} for y in range(0,baynet.output_size)]
-    for y_index in range(len(p_y)):
-        for y_value in baynet.model['Y'][y_index]:
-            p_y = (1.0*baynet.model['Y'][y_index][y_value])/baynet.model['samples']
-            p_x = 1.0
-            p_x_y = 1.0
-            for (x_index, x_value) in [(x_index, X[x_index]) for x_index in range(baynet.input_size)]:
-                p_x *= (1.0*baynet.model['X'][x_index][x_value])/baynet.model['samples']
-                p_x_y *= (1.0*baynet.model['X|Y'][y_index][y_value][x_index][x_value])/baynet.model['Y'][y_index][y_value]
-            p_y[y_index][y_value] = {
-                            'p_y': p_y,
-                            'p_x': p_x,
-                            'p_x_y': p_x_y,
-                            'value': (p_x_y*p_y/p_x),
-            }
-    return p_y
 
 
 if __name__ == "__main__":
