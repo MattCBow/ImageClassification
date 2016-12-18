76ff0cfdc2c14bd385eb8216db6376d72ee407a1
Matt Bowyer <mattbowyer@Matts-MacBook-Air-2.local>
2016-12-18 16:24:32 -0500

    fixed model trainer

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -16,7 +16,6 @@ class ModelTrainer(object):
 
     def callbackF(self, params):
         self.count+=1
-        print self.count, ' - finish'
         self.model.set_params(params)
         self.J.append(self.model.cost_function(self.X, self.y))
 
